93433ae2024a7b8f6bb9cede564e4a18e463839c
ishu kumar <ishukumar200706@gmail.com>
2026-06-02 00:37:17 -0700

    DSA LECTURE 7 TOPIC STRING ARRAY

---
+++ DSA_7TH_LECTURE/app.js
@@ -0,0 +1,71 @@
+
+// Q PRINT EACH CHARACTER ON NEW LINE 
+
+// let name = "ishu";
+
+// for(let i = 0; i<name.length; i++){
+//     console.log(name[i]);
+    
+// }
+
+
+
+
+// Q PRINT REVERSE ORDER
+
+// let name = "Ishu";
+
+// for(let i = name.length-1; i>=0; i--){
+//    process.stdout.write(`${name[i]}`)
+    
+// }
+
+
+
+
+// Q CHECK PALLINDROME
+
+// let str = "makdam";
+
+// let ispallindrome = true;
+
+// let i = 0 , j = str.length-1;
+
+// while(i<j){
+//     if(str[i]!==str[j]){
+//         ispallindrome = false;
+//         break;
+//     }
+//     i++
+//     j--
+// }
+// if(ispallindrome){
+//     console.log("pallindrome");
+    
+// }
+// else{
+//     console.log("No pallindrome");
+    
+// }
+
+
+
+
+// Q toogle the character 
+
+// let str = "Ishu KUmar";
+
+// let toogle = "";
+
+// for(let i = 0; i<str.length; i++){
+//     let ch = str.charCodeAt(i)
+
+//     if(ch>=65 && ch<=90){
+//         toogle = toogle + String.fromCharCode(ch + 32)
+//     }
+//     else if(ch>=97 && ch<=122){
+//         toogle = toogle + String.fromCharCode(ch - 32)
+
+//     }
+// }
+// console.log(toogle);
